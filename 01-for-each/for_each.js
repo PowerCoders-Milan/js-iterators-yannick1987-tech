2c6d889379e5f8f38e5693d6c11fcc87ec61d69d
yannick1987-tech <ngassamnanayannick8@gmail.com>
2021-06-22 17:44:57 +0200

    exerise 1 done

--- 01-for-each/for_each.js
+++ 01-for-each/for_each.js
@@ -1,5 +1,9 @@
 // define an array of fruits
-
+const array = ['melon','banana','mangoes','oranges']
 // .forEach((fruit, index) => {
     // console.log() a message interpolating the name of the fruit and it's position in the array (index increased by 1);
 // })
+array.forEach( (fruit,index)=> {
+    console.log(`${index+1} - ${fruit} `)
+    }
+);
